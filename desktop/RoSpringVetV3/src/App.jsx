68e6c6c378b5dcdb14cd91eb1aa52d786a4acb70
cristibadea777 <cristibadea777@gmail.com>
2023-09-24 23:56:51 +0300

    Creat aplicatie Tauri

---
+++ desktop/RoSpringVetV3/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import LoginRegister from "./components/LoginRegister/LoginRegister";
+
+function App() {
+  
+  //#232B2B #1e1e1e #11574a
+
+  return (
+    <div className="container-principal">
+      <LoginRegister/>
+    </div>
+  )
+}
+export default App;
